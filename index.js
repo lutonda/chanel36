285fe7c26fa2c7dd44dabfec07d14b36fa733d3b
Sebastião Dias Lutonda <lutonda@gmail.com>
2021-04-09 11:05:30 +0000

    new fixes

--- index.js
+++ index.js
@@ -10,33 +10,57 @@
  // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder=require('string_decoder').StringDecoder;
 
 
-// Functions 
-var helloFunction=function(res,req){
-        res.end(JSON.stringify({'message':'Hello! Welcome to my simple HTTP nodejs Application'}));    
-    }
-var unknowFunction=function(res,req){
-        res.end(JSON.stringify({'message':'unknow path'}));    
-    }
 
  // Respond to all request    
 var server=http.createServer(function(req,res){
 
 
     // getting and pasing the requested HTTP path
-    var path=url.parse(req.url,true).pathname;
-    var trimedPath=path.replace(/^\/+|\/+$/g,'');
-    
+    var parseUrl=url.parse(req.url,true);
+    var path=parseUrl.pathname;
+    var trimmedPath=path.replace(/^\/+|\/+$/g,'');
+    var queryString=parseUrl.query;
+    var method=req.method.toLower;
+    var headers=req.headers;
+
+    var decoder= new StringDecoder('utf-8');
+    var buffer='';
+    req.on('data',function(data){
+        buffer+=decoder.write(data)
+    })
+    req.on('end',function(){
+        buffer+=decoder.end()
+
+        var chosenHandler=typeof(handlers[trimmedPath])!='undefined' ? handlers[trimmedPath] : handlers.notfound
+
+        var data={
+            'trimmedPath':trimmedPath,
+            'queryString':queryString,
+            'method':method,
+            'headers':headers,
+            'payload':buffer
+        }
+
+
+        console.log(typeof(handlers[trimmedPath])!=='undefined');
+        chosenHandler(data,function(statusCode, payload){
+            statusCode=typeof(statusCode)==='number' ? statusCode : 200;
+
+            payload=typeof(payload)==='object' ? payload : {};
+
+            var payloadString=JSON.stringify(payload);
+
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log('Returnig this reponse: ', statusCode, payloadString)
+        })
+    })
     // Telling the client that we are sending JSON
     res.setHeader('Content-Type','application/json')
-    
-    // check the path
-    if(trimedPath=='hello'){
-        helloFunction(res,req)
-    }else{
-        unknowFunction(res,req)
-    }
     
 })
 
@@ -45,3 +69,18 @@ server.listen('3000',function(){
     console.log('Server listning on port 3000')
 })
 
+
+var handlers={}
+
+handlers.hello=function(data,callback){
+    callback(200, {message:'Hello! Welcome to my simple HTTP nodejs Application'})
+}
+
+
+handlers.notfound=function(data,callback){
+    callback(404)
+}
+
+var router={
+    'hello' : handlers.hello
+}
